0f6ffdccc5853875316fccaf95c6ce9f4ee42087
chenshuai <1448509198@qq.com>
2019-09-27 16:49:34 +0800

    第二次修改项目添加进本地仓库

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -4,4 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello git class");
     }
+
+    public void test(){
+        int i=0;
+        System.out.println(i);
+    }
 }
